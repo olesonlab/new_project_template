3420ef4b602dfa5f9c30e51e6ee21bb700afe41a
alemarieceria <alemarie@hawaii.edu>
2024-09-06 01:04:21 -1000

    Fix errors in install_packages_and_load_libraries <- function(cran_packages = NULL, github_packages = NULL) {

--- functions/install_packages_and_load_libraries.R
+++ functions/install_packages_and_load_libraries.R
@@ -1,40 +1,45 @@
 install_packages_and_load_libraries <- function(cran_packages = NULL, github_packages = NULL) {
-  # Function to install a package if not already installed
-  ensure_package_installed <- function(pkg) {
-    if (!requireNamespace(pkg, quietly = TRUE)) {
-      install.packages(pkg)
-    }
+  
+  # Ensure pacman is installed first
+  if (!requireNamespace("pacman", quietly = TRUE)) {
+    install.packages("pacman")
   }
   
-  # Ensure pacman and renv are installed
-  ensure_package_installed("pacman")
-  ensure_package_installed("renv")
-  # 
-  # Load pacman and renv
+  # Load pacman
   library(pacman)
-  library(renv)
   
   # Install and load CRAN packages if specified
   if (!is.null(cran_packages) && length(cran_packages) > 0) {
-    pacman::p_load(char = cran_packages)
+    pacman::p_load(char = cran_packages)  # Automatically installs and loads packages
   }
   
   # Install and load GitHub packages if specified
   if (!is.null(github_packages) && length(github_packages) > 0) {
-    pacman::p_install_gh(github_packages)
+    pacman::p_load_gh(github_packages)  # Automatically installs and loads GitHub packages
   }
+  
+  # Ensure renv is installed and loaded
+  if (!requireNamespace("renv", quietly = TRUE)) {
+    install.packages("renv")
+  }
+  library(renv)
+  
+  # Quietly take a snapshot of the current environment to track dependencies
+  suppressMessages(invisible(capture.output(renv::snapshot())))
 }
 
-# Example usages. Add or remove packages as needed. Then source file in quarto
+# Example usages. 
+# Add or remove packages as needed. Then source file in quarto
 # docs so that dependencies are managed within one area.
 cran_packages <- c(
   # File management
-  "fs", "here", "utils", "tools",
+  "fs", "here", "utils", "tools", 
   # Data tidying
-  "tidyverse", "janitor", "scales",
+  "tidyverse", "janitor", "scales", "forcats",
   # Data visualization
-  "plotly", "DT", "gt", "patchwork", "wesanderson", "ggtext", "ggiraph", 
-  "rmarkdown", "leaflet", "leaflet.esri", "shiny", "gridExtra",
+  "plotly", "DT", "gt", "patchwork", "ggokabeito", "ggtext", "ggiraph", 
+  "rmarkdown", "leaflet", "leaflet.esri", "shiny", "gridExtra", "ggthemes",
+  "mapview",
   # Spatial analysis
   "terra", "sf", "units")
 
@@ -44,6 +49,3 @@ install_packages_and_load_libraries(
   cran_packages = cran_packages
   # github_packages = github_packages
 )
-
-# Snapshot the renv environment
-suppressMessages(invisible(capture.output(renv::snapshot()))) 
